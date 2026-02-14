4d51667cb792053a9b49f1fc72e35f2a927c963f
zahra <zahra.st7373@gmail.com>
2026-02-14 10:29:04 +0330

    refactor: improve formatting and organization of Swagger setup in gateway application

--- apps/gateway/config/config.swagger.ts
+++ apps/gateway/config/config.swagger.ts
@@ -4,59 +4,61 @@ import { AdminModule } from "../src/rest/admin/admin.module";
 import { DriverModule } from "../src/rest/driver/driver.module";
 import { PassengerModule } from "../src/rest/passenger/passenger.module";
 interface SwaggerModuleItem {
-    path:string;
-    module?:any;
-    bearer?:boolean;
+    path: string;
+    module?: any;
+    bearer?: boolean;
 }
 
-export function setupSwagger(app:INestApplication , cfg) {
-const apiVersion = cfg.get('server.apiVersion');
-const swaggerTitle = cfg.get('server.swagger.title');
-const swaggerDescription = cfg.get('server.swagger.description');
-const swaggerVersion = cfg.get('server.swagger.version');
+export function setupSwagger(app: INestApplication, cfg) {
+    const apiVersion = cfg.get('server.apiVersion');
+    const swaggerTitle = cfg.get('server.swagger.title');
+    const swaggerDescription = cfg.get('server.swagger.description');
+    const swaggerVersion = cfg.get('server.swagger.version');
 
-const mainOptions = new DocumentBuilder()
-    .setTitle(`${swaggerTitle} - Admin`)
-    .setDescription(swaggerDescription)
-    .setVersion(swaggerVersion)
-    .addBearerAuth()
-    .build();
-
-const mainDocument = SwaggerModule.createDocument(app, mainOptions , {
-    include: [AdminModule]
-});
-SwaggerModule.setup(`/${apiVersion}/docs`, app, mainDocument);
-const modules:SwaggerModuleItem[] = [
-    { 
-        path: 'admin',
-        module: AdminModule,
-        bearer: false,
-    },
-    { 
-        path: 'driver',
-        module: DriverModule,
-        bearer: true,
-    },
-    { 
-        path: 'passenger',
-        module: PassengerModule,
-        bearer: true,
-    },
-];
-modules.forEach(({path, module, bearer}) => {
-   let options = new DocumentBuilder()
-        .setTitle(`${swaggerTitle} - ${path}`)
+    const mainOptions = new DocumentBuilder()
+        .setTitle(`${swaggerTitle} - Admin`)
         .setDescription(swaggerDescription)
         .setVersion(swaggerVersion)
-       if(bearer) {
-        options = options.addBearerAuth(
-            {type: 'http',
-            scheme: 'bearer',  
-            bearerFormat: 'JWT',
-            name: 'Authorization',
-            in: 'header'},
-        );
-       }
-});
+        .addBearerAuth()
+        .build();
+
+    const mainDocument = SwaggerModule.createDocument(app, mainOptions, {
+        include: [AdminModule]
+    });
+    SwaggerModule.setup(`/${apiVersion}/docs`, app, mainDocument);
+    const modules: SwaggerModuleItem[] = [
+        {
+            path: 'admin',
+            module: AdminModule,
+            bearer: false,
+        },
+        {
+            path: 'driver',
+            module: DriverModule,
+            bearer: true,
+        },
+        {
+            path: 'passenger',
+            module: PassengerModule,
+            bearer: true,
+        },
+    ];
+    modules.forEach(({ path, module, bearer }) => {
+        let options = new DocumentBuilder()
+            .setTitle(`${swaggerTitle} - ${path}`)
+            .setDescription(swaggerDescription)
+            .setVersion(swaggerVersion)
+        if (bearer) {
+            options = options.addBearerAuth(
+                {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                    name: 'Authorization',
+                    in: 'header'
+                },
+            );
+        }
+    });
 }
 
